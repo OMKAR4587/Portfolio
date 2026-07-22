3d4bed6c273fda44bc6e49988b1adbcbb25ad1a4
Omkar Yevale <omee5663@gmail.com>
2026-07-22 22:22:00 +0530

    the home and project section responsiveness has completed

--- src/hooks/ProjectCardAnimation.jsx
+++ src/hooks/ProjectCardAnimation.jsx
@@ -381,92 +381,192 @@ const ProjectCardAnimation = (sectionRef, projectRef) => {
 
         });
 
-       mm.add("(max-width:768px)", () => {
+        mm.add("(max-width:768px)", () => {
 
-    // Reset all desktop styles
-    gsap.set(projectRef.current, {
-        clearProps: "all",
-    });
+             gsap.set(projectRef.current, {
+                position: "absolute",
+                top: "4.5%",
+                left: "50%",
+                xPercent: -50,
+                yPercent: -50,
+                transformOrigin: "center center",
+            });
 
-    gsap.set(".Project-container", {
-        clearProps: "all",
-    });
+            gsap.set([
+                ".project-detail-container-1",
+                ".project-detail-container-2",
+                ".project-detail-container-3",
+            ], {
+                opacity: 0,
+            });
 
-    gsap.set(".project-container-overlay", {
-        clearProps: "all",
-    });
+            // ---------------- MASTER TIMELINE ----------------
 
-    gsap.set([
-        ".project-holder-1",
-        ".project-holder-2",
-        ".project-holder-3"
-    ], {
-        clearProps: "all",
-        opacity: 1,
-        scale: 1,
-    });
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    trigger: ".Project-section",
+                    start: "14% bottom",
+                    end: "bottom bottom",
+                    pin: true,
+                    scrub: true,
+                    invalidateOnRefresh: true,
+                    // markers:true
+                },
+            });
 
-    gsap.set([
-        ".project-detail-container-1",
-        ".project-detail-container-2",
-        ".project-detail-container-3"
-    ], {
-        opacity: 1,
-    });
+           tl.to(".Project-container", {
+                y: -20,
+                duration: 1,
+            })
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".Project-section",
-            start: "top 75%",
-            end: "bottom bottom",
-            scrub: 1,
-            invalidateOnRefresh: true,
-        },
-    });
+                .to(".project-container-overlay", {
+                    clipPath: "inset(0% 0% round 30px)",
+                    duration: 2,
+                    ease: "none",
+                })
 
-    tl.from(".Project-container", {
-        opacity: 0,
-        y: 80,
-        duration: 1,
-    });
+                .to(".Project-container", {
+                    y: -240,
+                    duration: 2,
+                    ease: "none",
+                })
 
-    tl.from(".project-holder-1", {
-        opacity: 0,
-        y: 80,
-        duration: 1,
-    }, "-=0.5");
+                .to(
+                    ".project-detail-container-1",
+                    {
+                        opacity: 1,
+                        duration: 0.2,
+                    },
+                )
 
-    tl.from(".project-detail-container-1", {
-        opacity: 0,
-        y: 30,
-        duration: 0.6,
-    }, "<");
+                .to({}, {
+                    duration: 2
+                })
+                .to({}, {
+                    duration: 2
+                })
+                .to({}, {
+                    duration: 2
+                })
+                .to({}, {
+                    duration: 2
+                })
+                .to(
+                    ".project-container-overlay",
+                    {
+                        scale: 1.1,
+                        duration: 2,
+                        ease: "none",
+                    },
+                    "<"
 
-    tl.from(".project-holder-2", {
-        opacity: 0,
-        y: 80,
-        duration: 1,
-    });
+                )
+            tl.to(".my-project-2", {
+                y: 0,
+                height: "100%",
+                duration: 1.5,
+            }, "<")
 
-    tl.from(".project-detail-container-2", {
-        opacity: 0,
-        y: 30,
-        duration: 0.6,
-    }, "<");
+                .to(".project-holder-2", {
+                    scale: 1.1,
+                    duration: 2,
+                    ease: "none",
+                }, "<")
 
-    tl.from(".project-holder-3", {
-        opacity: 0,
-        y: 80,
-        duration: 1,
-    });
+                .to(
+                    ".project-holder-2",
+                    {
+                        clipPath: "inset(0% 0% 0% 0%)",
+                        duration: 1.5,
+                        ease: "none",
+                    },
+                    "<"
+                )
+                .to(
+                    ".project-detail-container-1",
+                    {
+                        opacity: 0,
+                        duration: 0.5,
+                    },
+                    "<"
+                )
 
-    tl.from(".project-detail-container-3", {
-        opacity: 0,
-        y: 30,
-        duration: 0.6,
-    }, "<");
+                .to(
+                    ".project-detail-container-2",
+                    {
+                        opacity: 1,
+                        duration: 0.5,
+                    },
+                    "<"
+                )
+                .to({}, {
+                    duration: 2
+                })
+                .to({}, {
+                    duration: 2
+                })
+                .to({}, {
+                    duration: 2
+                })
+                .to(".project-holder-2", {
+                    scale: 1,
+                    duration: 2,
+                    ease: "none",
+                }, "<")
 
-});
+                .to(
+                    ".project-holder-3",
+                    {
+                        clipPath: "inset(0% 0% 0% 0%)",
+                        duration: 1.5,
+                        ease: "none",
+                    },
+                    "<"
+                );
+
+            tl.fromTo(".project-holder-3", { scale: 1.1 }, {
+                scale: 1,
+                duration: 2,
+                ease: "none",
+            }, "<")
+            tl.to(
+                ".project-detail-container-2",
+                {
+                    opacity: 0,
+                    duration: 0.5,
+                },
+                "<"
+            )
+
+                .to(
+                    ".project-detail-container-3",
+                    {
+                        opacity: 1,
+                        duration: 0.5,
+                    },
+                    "<"
+                )
+                .to({}, {
+                    duration: 2
+                })
+                .to({}, {
+                    duration: 2
+                })
+                .to(
+                    ".project-detail-container-3",
+                    {
+                        opacity: 0,
+                        duration: 2
+                    },
+                    "<"
+                )
+                .to(".Project-container", {
+                    opacity: 0,
+                    duration: 2,
+                    ease: "none",
+                }, "<")
+
+        });
 
         // ---------------- INITIAL STATE ----------------
 
